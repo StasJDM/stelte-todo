d950487ce14c7f920d6a41564c51ed96d6d543a3
StasJDM <pyatnicyn.stanislav@gmail.com>
2022-01-22 01:42:07 +0300

    Created Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  let text;
+  export { text };
+
+  const dispatch = createEventDispatcher();
+
+  const onClick = () => {
+    dispatch('click');
+  };
+</script>
+
+<button on:click={onClick}>{text}</button>
+
+<style>
+  button {
+    margin: 0;
+    background: #ff3e00;
+    border-radius: 4px;
+    color: white;
+    font-weight: 600;
+    border: none;
+  }
+
+  button:hover {
+    background: #ff5d27;
+  }
+
+  button:active {
+    background: #c03000;
+  }
+</style>
