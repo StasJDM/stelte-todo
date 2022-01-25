f49767a7a8533afe727c81ab6a2dcc2c8150ea14
StasJDM <pyatnicyn.stanislav@gmail.com>
2022-01-26 00:43:40 +0300

    First implementation of export-dialog

---
+++ src/dialogs/export-dialog.svelte
@@ -0,0 +1,29 @@
+<div class="modal-background" />
+<div class="modal-container">Modal!</div>
+
+<style>
+  .modal-background {
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .modal-container {
+    width: 480px;
+    height: 200px;
+    z-index: 2;
+    position: fixed;
+    margin: auto;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    border: 1px solid #dadada;
+    border-radius: 4px;
+    box-shadow: 2px 2px 10px #bdbdbd;
+  }
+</style>
